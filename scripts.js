4b4d53be68d47fe8551e3a0830eea39c5e423b59
ISich <arseny.voroni@yandex.ru>
2024-05-28 21:45:27 +0500

    add jpg

--- scripts.js
+++ scripts.js
@@ -39,14 +39,34 @@ function showSingleButton() {
     for(const r of tracks) {
         console.log(r);
     }
-    const track1 = tracks[0];
+    const ans = getRandomInt(4);
+
+    const track_ans = tracks[0];
+    const track1 = tracks[1];
+    const track2 = tracks[2];
+    const track3 = tracks[3];
     //const track1 = (getRandomInt(4) + 1).toString();
     document.getElementById('buttonContainer').innerHTML = `
-        <button onclick="toggleMusic('music/${track1}.mp3')">Play/Pause Track 1</button>
-        <button onclick="makeRed(this)">Choose botton 1</button>
-        <button onclick="makeGreen(this)">Choose button 2</button>
-        <button onclick="makeRed(this)">Choose button 3</button>
+        <button onclick="toggleMusic('music/${track_ans}.mp3')">Play/Pause Track 1</button>
+
+        <button class="image-button" onclick="toggleButtonBackground(this, 'green-background')">
+        <img src="cover/${track_ans}.jpg" alt="Play Music" width="100px" height="100px">
+        </button>
+
+        <button class="image-button" onclick="toggleButtonBackground(this, 'red-background')">
+        <img src="cover/${track1}.jpg" alt="Play Music" width="100px" height="100px">
+        </button>
+
+        <button class="image-button" onclick="toggleButtonBackground(this, 'red-background')">
+        <img src="cover/${track2}.jpg" alt="Play Music" width="100px" height="100px">
+        </button>
+
+        <button class="image-button" onclick="toggleButtonBackground(this, 'red-background')">
+        <img src="cover/${track3}.jpg" alt="Play Music" width="100px" height="100px">
+        </button>
+
         <button onclick="showMultipleButtons()">Go menu</button>
+
         <img src="cover/0.jpg" width="100" height="100" />
     `;
 }
@@ -67,6 +87,11 @@ function makeRed(button) {
 
 function makeGreen(button) {
     button.classList.add('green-button');
+}
+
+
+function toggleButtonBackground(button, colorClass) {
+    button.classList.toggle(colorClass);
 }
 
 window.onload = showMultipleButtons;
