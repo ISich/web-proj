b31571ec56c5b852421b5fd9699a634c15011870
kulikovartem <artemkulikovsprog@gmail.com>
2024-06-05 01:09:29 +0500

    Add second html page

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import settings
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = settings.DATABASE_URL_psycopg
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
--- models.py
+++ models.py
@@ -1,12 +1,9 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import ForeignKey
+from main import db
 
 
-class Base(DeclarativeBase):
-    pass
-
-
-class Tracks(Base):
+class Tracks(db.model):
     __tablename__ = 'tracks'
 
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
@@ -14,14 +11,14 @@ class Tracks(Base):
     type: Mapped[str]
 
 
-class Singers(Base):
+class Singers(db.model):
     __tablename__ = 'singers'
 
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
     name: Mapped[str]
 
 
-class TracksBySingers(Base):
+class TracksBySingers(db.model):
     __tablename__ = 'tracksbysingers'
 
     track_id: Mapped[int] = mapped_column(ForeignKey('tracks.id'), primary_key=True)
